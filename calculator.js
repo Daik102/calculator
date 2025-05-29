3b0869972b794ae415b55710e0bd7ec482978ed0
Daik <fruit@kd6.so-net.ne.jp>
2025-05-30 00:45:41 +0900

    Refactoring operate function

--- calculator.js
+++ calculator.js
@@ -2,8 +2,8 @@ const title = document.querySelector('.title');
 const displayContainer = document.querySelector('.display-container');
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
-const allClear = document.querySelector('.all-clear');
 const backSpace = document.querySelector('.back-space');
+const allClear = document.querySelector('.all-clear');
 const footer = document.querySelector('.footer');
 const factor = 10000;
 let firstNum = '';
@@ -15,127 +15,39 @@ let freezed;
 let matrix;
 let hello;
 
-title.addEventListener('click', () => {
-  if (matrix) {
-    return;
-  }
-  if (hello) {
-    if (secondNum !== '') {
-      display.textContent = secondNum;
-    } else if (firstNum !== '') {
-      display.textContent = firstNum;
-    } else {
-      display.textContent = '0';
-    }
-    hello = false;
-    return;
-  }
-  display.textContent = 'Hello';
-  hello = true;
-});
-
-function add(a, b) {
-  const sum = ((Number(a) * factor + Number(b) * factor) / factor).toString();
-  if ((sum) >= 1000000000000) {
+function handleLargeDigits(result) {
+  if ((result) >= 1000000000000) {
     freezed = true;
     return 'Over trillion!';
-  } 
-  if (sum.length >= 13) {
-    const twelveDigit = sum.slice(0, 12);
-    if (twelveDigit.slice(-1) === '.') {
-      return twelveDigit.slice(0, 11);
-    } else {
-      return twelveDigit;
-    }
-  } else {
-    return sum;
   }
-}
-
-function subtract(a, b) {
-  const difference = ((a * factor - b * factor) / factor).toString();
-  if ((difference) <= -1000000000000) {
+  if ((result) <= -1000000000000) {
     freezed = true;
     return 'Under trillion!';
   }
-  if (difference.length >= 14) {
-    const thirteenDigit = difference.slice(0, 13);
-    if (thirteenDigit.slice(-1) === '.') {
-      return thirteenDigit.slice(0, 12);
-    } else {
-      return thirteenDigit;
+  if (result.includes('-')) {
+    if (result.length >= 14) {
+      result = result.slice(0, 13);
+      if (result.slice(-1) === '.') {
+        result = result.slice(0, 12);
+      }
     }
-  } else {
-    return difference;
-  }
-}
-
-function multiply(a, b) {
-  const product = (((a * factor) * (b * factor)) / (factor * factor)).toString();
-  if ((product) >= 1000000000000) {
-    freezed = true;
-    return 'Over trillion!';
-  } 
-  if ((product) <= -1000000000000) {
-    freezed = true;
-    return 'Under trillion!';
-  }
-  if (product.includes('-') && product.length >= 14) {
-    const thirteenDigit = product.slice(0, 13);
-    if (thirteenDigit.slice(-1) === '.') {
-      return thirteenDigit.slice(0, 12);
-    } else {
-      return thirteenDigit;
+  } else if (result.length >= 13) {
+    result = result.slice(0, 12);
+    if (result.slice(-1) === '.') {
+      result = result.slice(0, 11);
     }
-  } else if (product.length >= 13) {
-    const twelveDigit = product.slice(0, 12);
-    if (twelveDigit.slice(-1) === '.') {
-      return twelveDigit.slice(0, 11);
-    } else {
-      return twelveDigit;
-    }
-  } else {
-    return product;
   }
-}
-
-function divide(a, b) {
-  const quotient = (((a * factor) / (b * factor))).toString();
-  if ((quotient) >= 1000000000000) {
-    freezed = true;
-    return 'Over trillion!';
-  } 
-  if ((quotient) <= -1000000000000) {
-    freezed = true;
-    return 'Under trillion!';
-  }
-  if (quotient.includes('-') && quotient.length >= 14) {
-    const thirteenDigit = quotient.slice(0, 13);
-    if (thirteenDigit.slice(-1) === '.') {
-      return thirteenDigit.slice(0, 12);
-    } else {
-      return thirteenDigit;
-    }
-  } else if (quotient.length >= 13) {
-    const twelveDigit = quotient.slice(0, 12);
-    if (twelveDigit.slice(-1) === '.') {
-      return twelveDigit.slice(0, 11);
-    } else {
-      return twelveDigit;
-    }
-  } else {
-    return quotient;
-  }
+  return result;
 }
 
 function operate() {
   if (operator === '/' && secondNum === '0') {
-    display.textContent = 'Welcome to the Matrix';
     matrix = true;
     freezed = true;
     document.body.classList.add('white-world');
-    displayContainer.classList.add('white-display');
+    displayContainer.classList.add('matrix-display');
     display.classList.add('small-font');
+    display.textContent = 'Welcome to the Matrix';
     buttons.forEach(button => {
       button.classList.add('white-world');
     });
@@ -148,14 +60,15 @@ function operate() {
     return;
   }
   if (operator === '+') {
-    result = add(firstNum, secondNum);
+    result = ((Number(firstNum) * factor + Number(secondNum) * factor) / factor).toString();
   } else if (operator === '-') {
-    result = subtract(firstNum, secondNum);
+    result = ((firstNum * factor - secondNum * factor) / factor).toString();
   } else if (operator === '*') {
-    result = multiply(firstNum, secondNum);
+    result = (((firstNum * factor) * (secondNum * factor)) / (factor * factor)).toString();
   } else if (operator === '/') {
-    result = divide(firstNum, secondNum);
+    result = (((firstNum * factor) / (secondNum * factor))).toString();
   }
+  result = handleLargeDigits(result);
   firstNum = result;
   operator = '';
   secondNum = '';
@@ -163,63 +76,95 @@ function operate() {
   display.textContent = firstNum;
 }
 
+function doAllClear() {
+  if (matrix) {
+    matrix = false;
+    document.body.classList.remove('white-world');
+    displayContainer.classList.remove('matrix-display');
+    display.classList.remove('small-font');
+    buttons.forEach(button => {
+      button.classList.remove('white-world');
+    });
+    allClear.textContent = 'AC';
+    backSpace.classList.remove('blue-pill');
+    backSpace.textContent = 'BS';
+    title.style.visibility = 'visible';
+    footer.style.visibility = 'visible';
+  }
+  firstNum = '';
+  operator = '';
+  secondNum = '';
+  result = '';
+  onSecondNum = false;
+  freezed = false;
+  display.textContent = '0';
+}
+
+function doBackSpace() {
+  if (firstNum === '' && !operator) {
+    return;
+  }
+  if (firstNum === result && !operator) {
+    firstNum = '';
+    display.textContent = '0';
+    return;
+  }
+  if (operator && secondNum === '') {
+    operator = '';
+    onSecondNum = false;
+  }
+  if (!onSecondNum) {
+    if (firstNum.length === 1) {
+      firstNum = '';
+      display.textContent = '0';
+    } else {
+      firstNum = firstNum.slice(0, firstNum.length -1);
+      display.textContent = firstNum;
+    }
+  } else {
+    if (secondNum.length === 1) {
+      secondNum = ''
+      display.textContent = '0';
+    } else {
+      secondNum = secondNum.slice(0, secondNum.length -1);
+      display.textContent = secondNum;
+    }
+  }
+}
+
+function handleDecimal() {
+  if (!onSecondNum) {
+    if (firstNum === '' || firstNum === result && !operator) {
+      firstNum = '0';
+    }
+    if (firstNum.includes('.')) {
+      return;
+    }
+    firstNum += '.';
+    display.textContent = firstNum;
+  } else {
+    if (secondNum === '') {
+      secondNum = '0';
+    }
+    if (secondNum.includes('.')) {
+      return;
+    }
+    secondNum += '.';
+    display.textContent = secondNum;
+  }
+}
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     if (button.className.includes('all-clear')) {
-      if (matrix) {
-        matrix = false;
-        document.body.classList.remove('white-world');
-        displayContainer.classList.remove('white-display');
-        display.classList.remove('small-font');
-        buttons.forEach(button => {
-          button.classList.remove('white-world');
-        });
-        allClear.textContent = 'AC';
-        backSpace.classList.remove('blue-pill');
-        backSpace.textContent = 'BS';
-        title.style.visibility = 'visible';
-        footer.style.visibility = 'visible';
-      }
-      firstNum = '';
-      operator = '';
-      secondNum = '';
-      result = '';
-      onSecondNum = false;
-      freezed = false;
-      display.textContent = '0';
+      doAllClear();
       return;
     }
     if (freezed) {
       return;
     }
     if (button.className.includes('back-space')) {
-      if (firstNum === '' && !operator) {
-        return;
-      }
-      if (firstNum === result && !operator) {
-        firstNum = '0';
-        display.textContent = firstNum;
-        return;
-      }
-      if (operator && secondNum === '') {
-        operator = '';
-        onSecondNum = false;
-      }
-      if (!onSecondNum) {
-        if (firstNum.length === 1) {
-          firstNum = '0';
-        } else {
-          firstNum = firstNum.slice(0, firstNum.length -1);
-        }
-        display.textContent = firstNum;
-      } else {
-        if (secondNum.length === 1) {
-          secondNum = '0';
-        } else {
-          secondNum = secondNum.slice(0, secondNum.length -1);
-        }
-        display.textContent = secondNum;
-      }
+      doBackSpace();
     }
     if (button.className.includes('number')) {
       if (!onSecondNum) {
@@ -254,25 +199,7 @@ buttons.forEach(button => {
       }
     }
     if (button.className.includes('decimal')) {
-      if (!onSecondNum) {
-        if (firstNum === '' || firstNum === result && !operator) {
-          firstNum = '0';
-        }
-        if (firstNum.includes('.')) {
-          return;
-        }
-        firstNum += '.';
-        display.textContent = firstNum;
-      } else {
-        if (secondNum === '') {
-          secondNum = '0';
-        }
-        if (secondNum.includes('.')) {
-          return;
-        }
-        secondNum += '.';
-        display.textContent = secondNum;
-      }
+      handleDecimal();
     }
     if (button.className.includes('operator')) {
       if (firstNum === '') {
@@ -281,10 +208,10 @@ buttons.forEach(button => {
       if (secondNum) {
         operate();
       }
-      if (button.className.includes('multiply')) {
-        operator = '*';
-      } else if (button.className.includes('divide')) {
+      if (button.className.includes('divide')) {
         operator = '/';
+      } else if (button.className.includes('multiply')) {
+        operator = '*';
       } else if (button.className.includes('subtract')) {
         operator = '-';
       } else if (button.className.includes('add')) {
@@ -302,61 +229,14 @@ buttons.forEach(button => {
 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Delete') {
-    if (matrix) {
-      matrix = false;
-      document.body.classList.remove('white-world');
-      displayContainer.classList.remove('white-display');
-      display.classList.remove('small-font');
-      buttons.forEach(button => {
-        button.classList.remove('white-world');
-      });
-      allClear.textContent = 'AC';
-      backSpace.classList.remove('blue-pill');
-      backSpace.textContent = 'BS';
-      title.style.visibility = 'visible';
-      footer.style.visibility = 'visible';
-    }
-    firstNum = '';
-    operator = '';
-    secondNum = '';
-    result = '';
-    onSecondNum = false;
-    freezed = false;
-    display.textContent = '0';
+    doAllClear();
     return;
   }
   if (freezed) {
     return;
   }
   if (e.key === 'Backspace') {
-    if (firstNum === '' && !operator) {
-      display.textContent = '0';
-      return;
-    }
-    if (firstNum === result && !operator) {
-      firstNum = '0';
-      display.textContent = firstNum;
-      return;
-    }
-    if (operator && secondNum === '') {
-      operator = '';
-      onSecondNum = false;
-    }
-    if (!onSecondNum) {
-      if (firstNum.length === 1) {
-        firstNum = '0';
-      } else {
-        firstNum = firstNum.slice(0, firstNum.length -1);
-      }
-      display.textContent = firstNum;
-    } else {
-      if (secondNum.length === 1) {
-        secondNum = '0';
-      } else {
-        secondNum = secondNum.slice(0, secondNum.length -1);
-      }
-      display.textContent = secondNum;
-    }
+    doBackSpace();
   }
 
   const numberKeys = '0123456789';
@@ -394,25 +274,7 @@ document.addEventListener('keydown', (e) => {
     }
   }
   if (e.key === '.') {
-    if (!onSecondNum) {
-      if (firstNum === '' || firstNum === result && !operator) {
-        firstNum = '0';
-      }
-      if (firstNum.includes('.')) {
-        return;
-      }
-      firstNum += '.';
-      display.textContent = firstNum;
-    } else {
-      if (secondNum === '') {
-        secondNum = '0';
-      }
-      if (secondNum.includes('.')) {
-        return;
-      }
-      secondNum += '.';
-      display.textContent = secondNum;
-    }
+    handleDecimal();
   }
 
   const operatorSymbols = '+-*/';
